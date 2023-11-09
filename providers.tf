896b4c76fc831e781b4a6327ab7d469470d3c92c
Mike Pashchenko <mishapashenko@gmail.com>
2023-11-09 12:35:22 +0000

    #19 change structure of root module

---
+++ providers.tf
@@ -0,0 +1,32 @@
+terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "MikPashch"
+
+  #  workspaces {
+  #    name = "terrahouse"
+  #  }
+  #}
+  #cloud {
+  #  organization = "MikPashch"
+  #  workspaces {
+  #    name = "terrahouse"
+  #  }
+  #}
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.16.2"
+    }
+  }
+}
+
+provider "aws" {
+}
+provider "random" {
+  # Configuration options
+}
